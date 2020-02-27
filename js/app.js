80111e46e16c1ad7523e2535e260d18c098cdea0
Stacey Teltser <stacey.teltser@gmail.com>
2020-02-27 13:31:48 -0600

    wrapped first two questions in functions

--- js/app.js
+++ js/app.js
@@ -2,139 +2,147 @@
 
 var score = 0;
 
-var pets = prompt('Do I have any pets?').toLowerCase();
-// console.log('Pets at home: yes');
-while (pets === '') {
+function question1(){
+  var pets = prompt('Do I have any pets?').toLowerCase();
+  // console.log('Pets at home: yes');
+  while (pets === '') {
     pets = prompt('Please answer the question.').toLowerCase();
-}
-while (pets !== '') {
+  }
+  while (pets !== '') {
     if (pets === 'yes') {
-        alert('Correct! I have a dog.');
-        score++;
-        break;
+      alert('Correct! I have a dog.');
+      score++;
+      break;
     } else if (pets === 'no') {
-        alert('I\'m sorry, I do have a pet.');
-        break;
+      alert('I\'m sorry, I do have a pet.');
+      break;
     } else {
-        pets = prompt('Please answer yes or no.').toLowerCase();
+      pets = prompt('Please answer yes or no.').toLowerCase();
     }
+  }
 }
 
-var travel = prompt('Have I traveled internationally?').toLowerCase();
-// console.log('Been outside of America: yes');
-while (travel === '') {
+question1();
+
+function question2(){
+  var travel = prompt('Have I traveled internationally?').toLowerCase();
+  // console.log('Been outside of America: yes');
+  while (travel === '') {
     travel = prompt('Please answer the question.').toLowerCase();
-}
-while (travel !== '') {
+  }
+  while (travel !== '') {
     if (travel === 'yes') {
-        alert('Correct! I have been to Germany and Ireland.');
-        score++;
-        break;
+      alert('Correct! I have been to Germany and Ireland.');
+      score++;
+      break;
     } else if (travel === 'no') {
-        alert('I have. I love to travel!');
-        break;
+      alert('I have. I love to travel!');
+      break;
     } else {
-        travel = prompt('Please answer yes or no.').toLowerCase();
+      travel = prompt('Please answer yes or no.').toLowerCase();
     }
+  }
 }
+
+question2();
 
 var saxophone = prompt('Do I play any musical instruments?').toLowerCase();
 // console.log('Plays music: yes');
 while (saxophone === '') {
-    saxophone = prompt('Please answer the question.').toLowerCase();
+  saxophone = prompt('Please answer the question.').toLowerCase();
 }
 while (saxophone !== '') {
-    if (saxophone === 'yes') {
-        alert('Correct! I play saxophone.');
-        score++;
-        break;
-    } else if (saxophone === 'no') {
-        alert('I do! I play saxophone.');
-        break;
-    } else {
-        saxophone = prompt('Please answer yes or no.').toLowerCase();
-    }
+  if (saxophone === 'yes') {
+    alert('Correct! I play saxophone.');
+    score++;
+    break;
+  } else if (saxophone === 'no') {
+    alert('I do! I play saxophone.');
+    break;
+  } else {
+    saxophone = prompt('Please answer yes or no.').toLowerCase();
+  }
 }
 
 var home = prompt('Was I born in Wisconsin?').toLowerCase();
 // console.log('Born in WI: no');
 while (home === '') {
-    home = prompt('Please answer the question.').toLowerCase();
+  home = prompt('Please answer the question.').toLowerCase();
 }
 while (home !== '') {
-    if (home === 'yes') {
-        alert('Nope! I was born in Colorado.');
-        break;
-    } else if (home === 'no') {
-        alert('Correct! I was born in Colorado.');
-        score++;
-        break;
-    } else {
-        home = prompt('Please answer yes or no.').toLowerCase();
-    }
+  if (home === 'yes') {
+    alert('Nope! I was born in Colorado.');
+    break;
+  } else if (home === 'no') {
+    alert('Correct! I was born in Colorado.');
+    score++;
+    break;
+  } else {
+    home = prompt('Please answer yes or no.').toLowerCase();
+  }
 }
 
 var whatNext = prompt('Will I stay in Iowa forever?').toLowerCase();
 // console.log('Staying in Iowa: no');
 while (whatNext === '') {
-    whatNext = prompt('Please answer the question.').toLowerCase();
+  whatNext = prompt('Please answer the question.').toLowerCase();
 }
 while (whatNext !== '') {
-    if (whatNext === 'yes') {
-        alert('Nope! I want to move to Washington next.');
-        break;
-    } else if (whatNext === 'no') {
-        alert('Correct! Iowa is a stepping stone for me.');
-        score++;
-        break;
-    } else {
-        whatNext = prompt('Please answer yes or no.').toLowerCase();
-    }
+  if (whatNext === 'yes') {
+    alert('Nope! I want to move to Washington next.');
+    break;
+  } else if (whatNext === 'no') {
+    alert('Correct! Iowa is a stepping stone for me.');
+    score++;
+    break;
+  } else {
+    whatNext = prompt('Please answer yes or no.').toLowerCase();
+  }
 }
 
 var band;
 // console.log('Band members: 4');
 for (var i = 0; i < 5; i++) {
-    if (!band) {
-        band = prompt('How many members are in my band?');
-    } else {
-        if (!isNaN(Number(band))) {
-            console.log(!isNaN(Number(band)));
-            if (band > 4) {
-                band = prompt('That is too many. Try again!');
-            } else if (band < 4) {
-                band = prompt('Too low! Try again.');
-            } else {
-                alert('Correct! Two guitars, a drummer, and bass.');
-                score++;
-                break;
-            }
-        }
-        else {
-            alert('Answer with a number please.');
-            band = null;
-        }
+  if (!band) {
+    band = prompt('How many members are in my band?');
+  } else {
+    if (!isNaN(Number(band))) {
+      console.log(!isNaN(Number(band)));
+      if (band > 4) {
+        band = prompt('That is too many. Try again!');
+      } else if (band < 4) {
+        band = prompt('Too low! Try again.');
+      } else {
+        alert('Correct! Two guitars, a drummer, and bass.');
+        score++;
+        break;
+      }
     }
+    else {
+      alert('Answer with a number please.');
+      band = null;
+    }
+  }
 }
 if (band != 4) {
-    console.log(band);
-    alert('There are four people in my band.');
+  console.log(band);
+  alert('There are four people in my band.');
 }
 
 var answers = ['maine', 'florida', 'germany', 'ireland', 'colorado', 'holland'];
 // console.log('I have been to: maine, florida, colorado, germany, ireland, and holland.')
 for (var i = 0; i < 6; i++) {
-    var isValid = false;
-    var places = prompt('Where is somewhere I have travelled to?').toLowerCase();
-    for (var j = 0; j < answers.length; j++) {
-        if (places === answers[j]) {
-            isValid = true;
-        }
+  var isValid = false;
+  var places = prompt('Where is somewhere I have travelled to?').toLowerCase();
+  for (var j = 0; j < answers.length; j++) {
+    if (places === answers[j]) {
+      isValid = true;
     }
-    if (isValid) {
-        score++;
-        break;
-    }
+  }
+  if (isValid) {
+    score++;
+    break;
+  }
 }
 var output = alert('Yes! I have been to Maine, Florida, Colorado, Ireland, Germany, and Holland.');
 
